345784e75467eb9e51c9603cd73b073114cc2cc3
Adam <aevancho@me.com>
2020-08-22 09:32:55 -0400

    removed some useless project files

--- sfdcTools/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SfdctoolsConfig(AppConfig):
-    name = 'sfdcTools'
--- sfdcTools/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-# This urls.py comes second.
-# At this point: http://www.website.com/sfdcTools/ has already been reached.
-# '' will return a page with nothing after http://.../sfdcTools/
-urlpatterns = [
-    path('', views.sfdcTools, name='sfdcTools-home'),
-    path('about/', views.sfdcTools, name='sfdcTools-about')]
