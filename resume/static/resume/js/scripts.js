39193a459bae9787dc8ec6373c8c852415a9681e
Adam <aevancho@me.com>
2020-08-22 09:17:50 -0400

    add dark mode. fix pictures

--- resume/static/resume/js/scripts.js
+++ resume/static/resume/js/scripts.js
@@ -74,3 +74,14 @@
       });
     });
 })(jQuery); // End of use strict
+
+function dark_toggle() {
+    var el1 = document.getElementById("dark-reader");
+    if(el1.disabled) {
+        el1.disabled = false;
+        localStorage.setItem("darkreader", "enabled");
+    } else {
+        el1.disabled = true;
+        localStorage.setItem("darkreader", "disabled");
+    }
+}
